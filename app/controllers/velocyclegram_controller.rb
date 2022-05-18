c72e1bd67903094d6dabba21186cddf09d3396fa
KuritsuM <dyma9111@gmail.com>
2022-05-18 20:44:29 +0300

    Implemented registration and authorization

---
+++ app/controllers/velocyclegram_controller.rb
@@ -0,0 +1,5 @@
+class VelocyclegramController < ApplicationController
+  def index
+
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  attr_writer :login
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["username = :value OR email = :value", { :value => login }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+end
